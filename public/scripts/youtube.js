9f57110a87d433f4ced2928dea22a478c15b94d5
Anna Claire <acfields11@gmail.com>
2020-09-22 02:56:33 -0500

    link instead

--- public/scripts/youtube.js
+++ public/scripts/youtube.js
@@ -1,44 +1,56 @@
-var GoogleAuth;
-var SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
+let GoogleAuth;
+let SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
 
-// Load the API's client and auth2 modules.
-// Call the initClient function after the modules load.
-function handleClientLoad() {
+function handleClientLoad(){
+  // Load the API's client and auth2 modules.
+  // Call the initClient function after the modules load.
   gapi.load('client:auth2', initClient);
 }
 
-function initClient() {
+function initClient(){
+
   var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
+
+  // Initialize the gapi.client object, which app uses to make API requests.
+  // Get API key and client ID from API Console.
+  // 'scope' field specifies space-delimited list of access scopes.
   gapi.client.init({
+    // ENTER YOUTUBE API KEY HERE
     apiKey: 'AIzaSyCBGFfp1HUEDzpvxHMZteoavvwOkT0BnDM',
     clientId: '453081527146-m3dri8nd17pnjgrkh016odc8km3tr3tb.apps.googleusercontent.com',
-    discoveryDocs: [discoveryUrl],
-    scope: SCOPE,
-  }).then(function () {
+    'discoveryDocs': [discoveryUrl],
+    // ENTER YOUTUBE API CLIENT ID HERE
+    'scope': SCOPE
+  }).then(() => {
     GoogleAuth = gapi.auth2.getAuthInstance();
 
     // Listen for sign-in state changes.
     GoogleAuth.isSignedIn.listen(updateSigninStatus);
 
-    // Handle initial sign-in state. 
+    // Handle initial sign-in state. (Determine if user is already signed in.)
     var user = GoogleAuth.currentUser.get();
     setSigninStatus();
 
+    // Call handleAuthClick function when user clicks on
+    //      "Sign In/Authorize" button.
 
-    $('#sign-in-or-out-button').click(function () {
+
+
+    var a = document.getElementById('sign-in-or-out-button');
+    a.onclick = function () {
       handleAuthClick();
-    });
-    $('#revoke-access-button').click(function () {
-      revokeAccess();
-    });
+    }
   });
 }
 
 
-function handleAuthClick(){
+function handleAuthClick() {
   if (GoogleAuth.isSignedIn.get()) {
+    // User is authorized and has clicked 'Sign out' button.
     GoogleAuth.signOut();
+    clearSelection();
   } else {
+    // User is not signed in. Start Google auth flow.
     GoogleAuth.signIn();
   }
 }
@@ -47,12 +59,11 @@ function revokeAccess(){
   GoogleAuth.disconnect();
 }
 
-function setSigninStatus(isSignedIn) {
+function setSigninStatus(isSignedIn){
   const user = GoogleAuth.currentUser.get();
   let isAuthorized = user.hasGrantedScopes(SCOPE);
   if (isAuthorized) {
-    $('#sign-in-or-out-button').html('Sign out');
-    $('#revoke-access-button').css('display', 'inline-block');
+    document.querySelector('#sign-in-or-out-button').innerHTML = "Signed in"
 
     youtubeSignedIn = true;
     youtubeSignedIn && spotifySignedIn ? bothSignedIn = true : console.log("nao")
@@ -60,7 +71,7 @@ function setSigninStatus(isSignedIn) {
   }
 }
 
-function updateSigninStatus(isSignedIn){
+const updateSigninStatus = isSignedIn => {
   setSigninStatus();
 }
 
@@ -99,7 +110,7 @@ const createPlaylist = playlistData => {
     part: 'snippet,status',
     resource: {
       snippet: {
-        title: `${makeid()}`,
+        title: `Spotitube ${makeid()}`,
         description: 'Spotify playlist converted to Youtube playlist'
       },
       status: {
